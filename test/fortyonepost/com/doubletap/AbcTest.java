d0c67ffe143bb3ba9af2fdc3c92bdd9f324b7686
Phuc Dang <huuphuc.cntt@gmail.com>
2013-08-05 17:04:58 +0700

    update test/fortyonepost/com/doubletap/AbcTest.java

---
+++ test/fortyonepost/com/doubletap/AbcTest.java
@@ -0,0 +1,23 @@
+package fortyonepost.com.doubletap;
+
+import android.test.AndroidTestCase;
+
+public class AbcTest extends AndroidTestCase {
+
+	@Override
+	protected void setUp() throws Exception {
+		// TODO Auto-generated method stub
+		super.setUp();
+	}
+	public void testABC(){
+		boolean isOK = true;
+		assertTrue(isOK);
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		// TODO Auto-generated method stub
+		super.tearDown();
+	}
+
+}
