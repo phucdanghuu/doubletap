5b5c32411a60c0b93d0b9596cbe572e17b08d71c
Phuc Dang <huuphuc.cntt@gmail.com>
2013-08-06 14:39:16 +0700

    update buildstuff.sh

--- buildstuff.sh
+++ buildstuff.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 FAILED=0
-sudo chmod -R 777 ./*
+sudo mkdir /sdcard
+sudo chmod 777 /sdcard
+cd ./daisy-engine
 ant clean build-and-test
+if [ "$?" = 1 ]; then
+    echo "daisy-engine build failed!"
+    FAILED=1
+fi
+cd ../DaisyBookLister
+cp ../daisy-engine/lib/*.jar ./libs/
+if [ "$?" = 1 ]; then
+    echo "copy of the daisy-engine jar files failed."
+    FAILED=1
+fi
 ant clean debug
 if [ "$?" = 1 ]; then
    echo "Debug build of the DaisyBookLister app failed."
    FAILED=1
 fi 
 exit $FAILED
-
